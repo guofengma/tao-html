1c5bedf065a56b0c7c34dbb4533d6d79b7601b62
xuyangzhou <18202982908@163.com>
2017-11-13 09:29:43 +0800

    add navbar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -56,11 +56,14 @@
       </div>
       <HotDoctorList :list='arr'/>
     </div>
+    <!-- 导航栏 -->
+    <navbar/>
   </div>
 </template>
 
 <script>
 import HotDoctorList from './hotDoctorList/HotDoctorList'
+import navbar from './navbar'
 export default {
   name: "HelloWorld",
   data() {
@@ -70,7 +73,8 @@ export default {
     };
   },
   components: {
-    HotDoctorList
+    HotDoctorList,
+    navbar
   }
 };
 </script>
@@ -84,13 +88,26 @@ export default {
 @primary: #0032a7;
 @placeholder: #999;
 @border: rgb(238, 238, 238); 
+.home{
+  padding-bottom:2.5rem;
+  padding-top:3.2rem;
+}
 // 顶部tab栏
 .topbar {
   font-size: 0.9rem;
   color: #fff;
-  padding: 1rem 0.5rem;
   background: #3794fe;
   text-align: center;
+  padding-bottom: 0.5rem;
+  .title{
+    width:100%;
+    padding:1rem 0.5rem;
+    background: #3794fe;
+    position: fixed;
+    top:0;
+    left:0;
+    z-index: 999;
+  }
   .topbar_icon {
     width: 1rem;
     height: 1rem;
@@ -106,7 +123,6 @@ export default {
     border-radius: 1.5rem;
     background: @mainColor;
     margin: 0rem auto;
-    margin-top: 1.5rem;
     display: flex;
     justify-content: flex-start;
     align-items: center;
@@ -188,51 +204,5 @@ export default {
 .hot_doctor {
   margin: 0.35rem 0;
   background: #fff;
-  .doctor_list {
-    background: #fff;
-    padding: 0.75rem 0.6rem;
-    .avatar {
-      width: 2.75rem;
-      height: 2.75rem;
-      border-radius:100%;
-      border: 1px solid #3794fe;
-      margin-right: 0.8rem;
-      img{
-        width:100%;
-      }
-    }
-    .doctor_info{
-      overflow:hidden;
-      font-size:0.8rem;
-      color:@font1Color;
-      h2{
-        color:@fontColor;        
-        margin-bottom:0.6rem;
-        span{
-          margin-right:0.5rem;
-        }
-      }
-      h4{  
-        margin-bottom:0.6rem;
-      }
-      p{
-        line-height:1.3rem;
-        margin-bottom:0.8rem;
-      }
-    }
-    .server_item {
-      margin-left:3.5rem;
-      li {
-        width: 1.5rem;
-        height: 1.5rem;
-        margin-right: 1.8rem;
-        float:left;
-        img {
-          width: 100%;
-          height: 100%;
-        }
-      }
-    }
-  }
 }
 </style>
